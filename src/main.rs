048a7f985b6d8aaab0209c30ba5fb486e6dd3ed7
Daniele Olmisani <mad4j@users.noreply.github.com>
2021-01-17 08:27:31 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
-
-use structopt::StructOpt;
 use chrono::{DateTime, Local};
+use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "datetag", about = "display a custmizable date tag")]
+#[structopt(name = "datetag", about = "display a customizable date tag")]
 struct Opt {
     /// date tag format
-    #[structopt(short, long, default_value="%Y%m%d")]
+    #[structopt(short, long, default_value = "%Y%m%d")]
     format: String,
+
+    /// date value
+    #[structopt(short, long)]
+    date: Option<String>,
 }
 
 fn main() {
-
     // parse command-line parameters
     let opt = Opt::from_args();
 
-    let now: DateTime<Local> = Local::now();
+    //let date: DateTime<Local> = Local::now();
+    
+    let date: DateTime<Local> = match opt.date {
+        Some(v) => DateTime::parse_from_str(opt.date)?,
+        None => Local::now()
+    }
+    
 
-    print!("{}", now.format(&opt.format))
+    print!(
+        "{}",
+        date.format(&opt.format)
+    )
 }
