471d5132598b856c4a9e2884f9901ed530c8d475
Daniele Olmisani <daniele.olmisani@gmail.com>
2024-04-29 21:54:41 +0200

    Added support to "--style" formatting.

--- src/datetag.rs
+++ src/datetag.rs
@@ -1,6 +1,8 @@
 use clap::ValueEnum;
 
-#[derive(Debug, Clone, ValueEnum)]
+use crate::datestyle::DateStyle;
+
+#[derive(Debug, Clone, Copy, ValueEnum)]
 pub enum DateTagType {
     Y,
     /// yearly tags (e.g. 2022)
@@ -15,11 +17,32 @@ pub enum DateTagType {
 
 /// associate a specific string format to each value
 impl DateTagType {
-    pub fn get_format(&self) -> &str {
-        match *self {
-            DateTagType::Yearly | DateTagType::Y => "%Y",
-            DateTagType::Monthly | DateTagType::M => "%Y%m",
-            DateTagType::Daily | DateTagType::D => "%Y%m%d",
+    pub fn get_format(&self, style: DateStyle) -> &str {
+        match (self, style) {
+            (DateTagType::Y, DateStyle::Plain) => "%Y",
+            (DateTagType::Y, DateStyle::Dot) => "%Y",
+            (DateTagType::Y, DateStyle::Slash) => "%Y",
+            (DateTagType::Y, DateStyle::Colon) => "%Y",
+            (DateTagType::Yearly, DateStyle::Plain) => "%Y",
+            (DateTagType::Yearly, DateStyle::Dot) => "%Y",
+            (DateTagType::Yearly, DateStyle::Slash) => "%Y",
+            (DateTagType::Yearly, DateStyle::Colon) => "%Y",
+            (DateTagType::M, DateStyle::Plain) => "%Y%m",
+            (DateTagType::M, DateStyle::Dot) => "%Y.%m",
+            (DateTagType::M, DateStyle::Slash) => "%Y/%m",
+            (DateTagType::M, DateStyle::Colon) => "%Y:%m",
+            (DateTagType::Monthly, DateStyle::Plain) => "%Y%m",
+            (DateTagType::Monthly, DateStyle::Dot) => "%Y.%m",
+            (DateTagType::Monthly, DateStyle::Slash) => "%Y/%m",
+            (DateTagType::Monthly, DateStyle::Colon) => "%Y:%m",
+            (DateTagType::D, DateStyle::Plain) => "%Y%m%d",
+            (DateTagType::D, DateStyle::Dot) => "%Y.%m.%d",
+            (DateTagType::D, DateStyle::Slash) => "%Y/%m/%d",
+            (DateTagType::D, DateStyle::Colon) => "%Y:%m:%d",
+            (DateTagType::Daily, DateStyle::Plain) => "%Y%m%d",
+            (DateTagType::Daily, DateStyle::Dot) => "%Y.%m.%d",
+            (DateTagType::Daily, DateStyle::Slash) => "%Y/%m/%d",
+            (DateTagType::Daily, DateStyle::Colon) => "%Y:%m:%d",
         }
     }
 }
@@ -29,29 +52,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_get_format_year() {
+    fn test_get_format_year_plain() {
         // test year-related variants
         let d = DateTagType::Yearly;
-        assert!(d.get_format() == "%Y");
+        assert!(d.get_format(DateStyle::Plain) == "%Y");
         let d = DateTagType::Y;
-        assert!(d.get_format() == "%Y");
+        assert!(d.get_format(DateStyle::Plain) == "%Y");
     }
 
     #[test]
-    fn test_get_format_month() {
+    fn test_get_format_month_plain() {
         // test month-related variants
         let d = DateTagType::Monthly;
-        assert!(d.get_format() == "%Y%m");
+        assert!(d.get_format(DateStyle::Plain) == "%Y%m");
         let d = DateTagType::M;
-        assert!(d.get_format() == "%Y%m");
+        assert!(d.get_format(DateStyle::Plain) == "%Y%m");
     }
 
     #[test]
-    fn test_get_format_day() {
+    fn test_get_format_day_plain() {
         // test day-related variants
         let d = DateTagType::Daily;
-        assert!(d.get_format() == "%Y%m%d");
+        assert!(d.get_format(DateStyle::Plain) == "%Y%m%d");
         let d = DateTagType::D;
-        assert!(d.get_format() == "%Y%m%d");
+        assert!(d.get_format(DateStyle::Plain) == "%Y%m%d");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,41 @@
 //! A datetag is a label similar to:
 //! * 20201113
 //! * TEST_202008
+//! * 202404_rel
 //!
 //! datetag contains:
 //!  * an optional prefix label (e.g. 'TEST_')
-//!  * a date reference
+//!  * a date reference (e.g. 202404)
+//!  * an optional suffix label (e.g '_rel')
 //!
 //! datetag references belong to one of the following types:
 //!  * YEARLY (i.e. match the format '%Y')
 //!  * MONTHLY (i.e. match the format '%Y%m')
 //!  * DAILY (i.e. match the format '%Y%m&d')
+//! 
+//! datetag refereces can be plain formated (e.g. 20240424) or
+//! formatted using a separator character (i.e. '.', ':' or '/'):
+//!  * 2024.04.24
+//!  * 2024-04-24
+//!  * 2024/04/24
 //!
 //! It is possible to add/subtract an offset to specific datetag.
-//! The offset value reprensents:
+//! The offset value should be expressed in:
 //!  * years
 //!  * months
 //!  * days
 //! depending on the datetag type.
 //!
-//! It is possible to obtain the NOW datetag or provide a  reference date.
+//! In case of generation of a single datetag then the offset will
+//! be immediately added (or subtracted) to the given reference
+//! date. Otherwise, the first datetag will represents the given
+//! reference date and the offset will be will be used to increase
+//! subsequent datetags.
+//! 
+//! It is possible to obtain the NOW datetag or provide the current
+//! reference date.
 
+mod datestyle;
 mod datetag;
 mod utils;
 
@@ -28,6 +44,7 @@ use anyhow::{Context, Result};
 use chrono::{Local, NaiveDate};
 use clap::Parser;
 
+use datestyle::DateStyle;
 use datetag::DateTagType;
 
 #[derive(Debug, Parser)]
@@ -50,12 +67,16 @@ struct Opt {
     #[arg(value_enum, short, long, default_value = "m")]
     tag_type: DateTagType,
 
-    /// Tag prefix (e.g. 'LAB_202404)
+    /// Date tag style
+    #[arg(value_enum, short, long, default_value = "plain")]
+    style: DateStyle,
+
+    /// Tag prefix (e.g. 'LAB_202404')
     #[arg(short, long)]
     prefix: Option<String>,
 
-    /// Tag suffix (e.g. '202404_rel)
-    #[arg(short, long)]
+    /// Tag suffix (e.g. '202404_rel')
+    #[arg(short = 'x', long)]
     suffix: Option<String>,
 
     /// Date value (one of 'yyyymmdd', 'yyyymm', 'yyyy')
@@ -113,7 +134,7 @@ fn main() -> Result<()> {
         print!(
             "{}{}{}",
             prefix,
-            date.format(opt.tag_type.get_format()),
+            date.format(opt.tag_type.get_format(opt.style)),
             suffix
         );
 
