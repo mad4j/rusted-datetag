7ebd76afbd1a5910121b9ac40b63e54ad0da3fb4
Daniele Olmisani <daniele.olmisani@gmail.com>
2024-05-01 11:35:04 +0200

    Better build configuration.

--- build.rs
+++ build.rs
@@ -1,10 +1,11 @@
-use {
-    std::{env, io},
-    winres::WindowsResource,
-};
+use std::io;
+
+#[cfg(windows)] 
+use winres::WindowsResource;
 
 fn main() -> io::Result<()> {
-    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+
+    #[cfg(windows)] {
         WindowsResource::new()
             // This path can be absolute, or relative to your crate root.
             .set_icon("resources/icon.ico")
