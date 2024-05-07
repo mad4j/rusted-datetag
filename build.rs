baa4e144c95e55e4aba1ef4a990a8a4da209597d
Daniele Olmisani <daniele.olmisani@gmail.com>
2024-05-07 21:43:39 +0200

    Closes Issues #3

--- build.rs
+++ build.rs
@@ -8,7 +8,7 @@ fn main() -> io::Result<()> {
     #[cfg(windows)] {
         WindowsResource::new()
             // This path can be absolute, or relative to your crate root.
-            .set_icon("resources/icon.ico")
+            .set_icon("assets/icon.ico")
             .compile()?;
     }
     Ok(())
